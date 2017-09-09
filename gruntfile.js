36ce63b0fa3ecc48b379212eaee8abf6d125ab83
VisibleDucts <VisibleDucts@gmail.com>
2017-09-09 09:29:31 -0500

    Added gruntfile/gitignore

---
+++ gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	
+	var config = require('./.screeps.json')
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var password = grunt.option('password') || config.password;
+    var ptr = grunt.option('ptr') ? true : config.ptr
+	
+    
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: config.email,
+                password: config.password,
+                branch: config.branch,
+                ptr: config.ptr
+            },
+            dist: {
+                src: ['src/*.js']
+            }
+        }
+    });
+}
